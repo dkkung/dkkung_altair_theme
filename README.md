6602611abdd7332847b3ba54e12e1860c87ce620
dkkung <128324842+dkkung@users.noreply.github.com>
2025-01-06 22:31:01 -0500

    started scratchpad; theme now importable

---
+++ README.md
@@ -0,0 +1,1 @@
+A collection of light- and dark-themed vega/altair configurations for use as global themes for altair charts.
---
+++ custom_altair_themes.py
@@ -0,0 +1,36 @@
+import altair as alt
+
+'''
+Global configuration values.
+'''
+font = "Helvetica Neue"
+dimX = 300
+dimY = 300
+fontSize = dimX / 30
+
+'''
+Defining custom themes using global and
+rational configuration values. The theme
+must be added to the register uniquely
+for each function definition using the
+@ decorator to pass the function to the
+register.
+'''
+
+@alt.theme.register('custom_light', enable = False)
+def custom_light() -> alt.theme.ThemeConfig:
+    return {
+        "config": {
+            "view": {"continuousWidth": dimX, "continuousHeight": dimY},
+            "mark": {"color": "black", "fill": "black"},
+        }
+    }
+    
+@alt.theme.register('custom_dark', enable = True)
+def custom_dark() -> alt.theme.ThemeConfig:
+    return {
+        "config": {
+            "view": {"continuousWidth": dimX, "continuousHeight": dimY},
+            "mark": {"color": "black", "fill": "black"},
+        }
+    }
