837ef86ff0d989ee7a0dc37ec4b09846f800041c
dkkung <128324842+dkkung@users.noreply.github.com>
2025-01-13 00:34:32 -0500

    added options function to specify global chart configs

--- custom_altair_themes.py
+++ custom_altair_themes.py
@@ -1,35 +1,72 @@
 import altair as alt
 
-'''
+"""
 Global configuration values.
-'''
-font = "Helvetica Neue"
+"""
 dimX = 300
 dimY = 300
+font = "Helvetica Neue"
 fontSize = dimX / 30
+fontStyle = "UtlraLight"
+transparentBackground = False
+backgroundColor = "white"
 
-'''
+"""
 Defining custom themes using global and
 rational configuration values. The theme
 must be added to the register uniquely
 for each function definition using the
 @ decorator to pass the function to the
 register.
-'''
-@alt.theme.register('custom_light', enable = False)
-def custom_light() -> alt.theme.ThemeConfig:
-    return {
-        "config": {
-            "view": {"continuousWidth": dimX, "continuousHeight": dimY},
-            "mark": {"color": "black", "fill": "black"},
-        }
-    }
-    
-@alt.theme.register('custom_dark', enable = False)
+"""
+
+
+# alt.theme.options = {"test": "test"}
+def options(dark_mode: bool, grid: bool):
+    alt.theme.options = {} # must reset options to remove stale keys
+    alt.theme.options["grid"] = grid
+    alt.theme.options["dark_mode"] = dark_mode
+
+# @alt.theme.register('custom_light', enable = False)
+# def custom_light() -> alt.theme.ThemeConfig:
+#     return {
+#         "config": {
+#             "view": {"continuousWidth": dimX, "continuousHeight": dimY},
+#             "mark": {"color": "black", "fill": "black"},
+#         }
+#     }
+
+
+@alt.theme.register("custom_dark", enable=False)
 def custom_dark() -> alt.theme.ThemeConfig:
+    alt.theme.enable("custom_dark")
     return {
         "config": {
-            "view": {"continuousWidth": dimX, "continuousHeight": dimY},
+            "axis": {
+                "grid": False,
+                "labelFontStyle": fontStyle,
+                "labelFlush": True,
+            },
+            # "background": None if transparentBackground else backgroundColor,
+            "background": "red" if alt.theme.options["dark_mode"] else backgroundColor,
+            "font": {"family": font},
             "mark": {"color": "black", "fill": "black"},
+            "view": {
+                "fill": backgroundColor,
+                "continuousWidth": dimX,
+                "continuousHeight": dimY,
+            },
+            "group": {
+                "strokeOpacity": 0,  # remove top and right axis borders
+            },
         }
     }
+
+
+"""
+The functions should be consolidated into a
+single function, with ternary operators
+used to distinguish between configuration
+values using a dark_mode global variable.
+Maybe try using alt.theme.options?
+"""
