55d4fed29ff35caec5353fc31b0f04c240629200
dkkung <128324842+dkkung@users.noreply.github.com>
2025-01-14 00:44:11 -0500

    widths set; helvetica neue thin default font set

--- custom_altair_themes.py
+++ custom_altair_themes.py
@@ -10,17 +10,23 @@ register.
 """
 
 
-# alt.theme.options = {"test": "test"}
 def options(
-    dark_mode=False,
-    dimX=300,
-    dimY=300,
-    font="Helvetica Neue",
-    fontSize=15,
-    fontStyle="UltraLight",
-    grid=False,
-    transparentBackground=False,
+    axisWidth = 0.5,
     backgroundColor="white",
+    darkmode=False,
+    font="Helvetica Neue",
+    fontStyle="Regular",
+    fontWeight=200, # only multiples of 100
+    grid=False,
+    markFillColor="black",
+    markFillOpacity=0.9,
+    markStrokeColor="black",
+    markStrokeOpacity=1,
+    markStrokeWidth=0.5,
+    markSize=40,
+    ticks = True,
+    topAndRightBorder=False,
+    transparentBackground=False,
 ):
     """
     Set global configuration options for the custom theme.
@@ -28,41 +34,117 @@ def options(
     options to override the defaults.
     """
     alt.theme.options = {}  # must reset options to remove stale keys
+    alt.theme.options["axisWidth"] = axisWidth
+    alt.theme.options["backgroundColor"]=backgroundColor,
+    alt.theme.options["darkmode"] = darkmode
     alt.theme.options["grid"] = grid
+    alt.theme.options["font"] = font
     alt.theme.options["fontStyle"] = fontStyle
-    alt.theme.options["dark_mode"] = dark_mode
+    alt.theme.options["fontWeight"] = fontWeight
+    alt.theme.options["markFillColor"] = markFillColor
+    alt.theme.options["markFillOpacity"] = markFillOpacity
+    alt.theme.options["markStrokeColor"] = markStrokeColor
+    alt.theme.options["markStrokeOpacity"] = markStrokeOpacity
+    alt.theme.options["markStrokeWidth"] = markStrokeWidth
+    alt.theme.options["markSize"] = markSize
+    alt.theme.options["ticks"] = ticks
+    alt.theme.options["tickWidth"] = axisWidth
+    alt.theme.options["topAndRightBorder"] = topAndRightBorder
+    alt.theme.options["transparentBackground"]=transparentBackground,
 
 
 @alt.theme.register("custom", enable=False)
 def custom() -> alt.theme.ThemeConfig:
     opts = alt.theme.options
     return {
+        "background": None if opts["darkmode"] else "white", # background of the entire view
         "config": {
             "axis": {
+                "domain": True,
+                "domainColor": "white" if opts["darkmode"] else "black",
+                "domainWidth": opts["axisWidth"],
                 "grid": opts["grid"],
+                "gridColor": "lightGray" if opts["darkmode"] else "lightGray",
+                "gridOpacity": 0.5,
+                "gridWidth": opts["axisWidth"],
+                "labelColor": "white" if opts["darkmode"] else "black",
+                "labelFont": opts["font"],
                 "labelFontStyle": opts["fontStyle"],
-                "labelFlush": True,
+                "labelFontWeight": opts["fontWeight"],
+                "ticks": opts["ticks"],
+                "tickColor": "white" if opts["darkmode"] else "black",
+                "tickWidth": opts["axisWidth"],
+                "titleColor": "white" if opts["darkmode"] else "black",
+                "titleFont": opts["font"],
+                "titleFontStyle": opts["fontStyle"],
+                "titleFontWeight": opts["fontWeight"],
             },
-            # "background": None if transparentBackground else backgroundColor,
-            # "background": "red" if opts["dark_mode"] else backgroundColor,
-            # "font": {"family": font},
-            # "mark": {"color": "black", "fill": "black"},
-            # "view": {
-            #     "fill": backgroundColor,
-            #     "continuousWidth": dimX,
-            #     "continuousHeight": dimY,
-            # },
-            "group": {
-                "strokeOpacity": 0,  # remove top and right axis borders
+            "background": None if opts["transparentBackground"] else opts["backgroundColor"],
+            "font": opts["font"],
+            "legend": {
+                "labelColor": "white" if opts["darkmode"] else "black",
+                "labelFont": opts["font"],
+                "labelFontStyle": opts["fontStyle"],
+                "labelFontWeight": opts["fontWeight"],
+                "gradientStrokeColor": "white" if opts["darkmode"] else "black",
+                "gradientStrokeWidth": opts["axisWidth"],
+                "titleColor": "white" if opts["darkmode"] else "black",
+                "titleFont": opts["font"],
+                "titleFontStyle": opts["fontStyle"],
+                "titleFontWeight": opts["fontWeight"],
             },
+            "point": {
+                "fill": opts["markFillColor"],
+                "fillOpacity": opts["markFillOpacity"],
+                "size": opts["markSize"],
+                "stroke": opts["markStrokeColor"],
+                "strokeOpacity": opts["markStrokeOpacity"],
+                "strokeWidth": opts["markStrokeWidth"],
+            },
+            "range": {
+                "category": {
+                    "scheme": "pastel1",
+                },
+                "diverging": {
+                    "scheme": "pinkyellowgreen",
+                },
+                "heatmap": {
+                    "scheme": "pinkyellowgreen",
+                },
+                "ordinal": {
+                    "scheme": "pastel1",
+                },
+                "ramp": {
+                    "scheme": "plasma" if opts["darkmode"] else "viridis",
+                },
+            },
+            "square": {
+                "fill": opts["markFillColor"],
+                "fillOpacity": opts["markFillOpacity"],
+                "size": opts["markSize"],
+                "stroke": opts["markStrokeColor"],
+                "strokeOpacity": opts["markStrokeOpacity"],
+                "strokeWidth": opts["markStrokeWidth"],
+            },
+            "title": {
+                "color": "white" if opts["darkmode"] else "black",
+                "font": opts["font"],
+                "fontStyle": opts["fontStyle"],
+                "fontWeight": opts["fontWeight"],
+                "subtitleColor": "white" if opts["darkmode"] else "black",
+                "subtitleFont": opts["font"], 
+                "subtitleFontStyle": opts["fontStyle"],
+                "subtitleFontWeight": opts["fontWeight"],
+            },
+            "view": {
+                # "fill": None,
+                # "fill": backgroundColor,
+                # "continuousWidth": 300,
+                # "continuousHeight": 300,
+                "stroke": "white" if opts["darkmode"] else "black",
+                "strokeOpacity": 1 if opts["topAndRightBorder"] else 0,  # remove top and right axis borders
+                "strokeWidth": opts["axisWidth"],
+            },
+            "group": {},
         }
     }
-
-
-"""
-The functions should be consolidated into a
-single function, with ternary operators
-used to distinguish between configuration
-values using a dark_mode global variable.
-Maybe try using alt.theme.options?
-"""
