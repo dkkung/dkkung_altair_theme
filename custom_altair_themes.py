ec7a0f575005b0df3d2e6f0c2356ff8a027280af
dkkung <128324842+dkkung@users.noreply.github.com>
2025-03-08 21:51:53 -0500

    added support for data type-specific custom color palettes

--- custom_altair_themes.py
+++ custom_altair_themes.py
@@ -11,14 +11,14 @@ register.
 
 
 def options(
-    angledX = False,
+    angledX=False,
     axisWidth=0.50,
     darkmode=False,
-    dashedLine = False,
-    dashedRule = True,
+    dashedLine=False,
+    dashedRule=True,
     font="Helvetica Neue",
     fontStyle="Regular",
-    fontWeight=200,  # only multiples of 100
+    fontWeight=300,  # only multiples of 100
     grid=False,
     gridColor="darkGray",
     legend=True,
@@ -33,10 +33,10 @@ def options(
     ticks=True,
     topAndRightBorder=False,
     transparentBackground=False,
-    verticalY = False,
+    verticalY=False,
     viewBackgroundColor="white",
-    xTicks = True,
-    yTicks = True,
+    xTicks=True,
+    yTicks=True,
 ):
     """
     Set global configuration options for the custom theme.
@@ -71,27 +71,40 @@ def options(
     alt.theme.options["viewBackgroundColor"] = viewBackgroundColor
     alt.theme.options["xTicks"] = xTicks
     alt.theme.options["yTicks"] = yTicks
+    # alt.theme.options["colors"] = {
+    #     "nature": ["#4BA69F", "#C6A639", "#53A157", "#4A8A95", "#FCF6D1"],
+    #     "categoricalSequential": ["#C4EFEC", "#4BA69F"]
+    # }
 
 
 @alt.theme.register("custom", enable=False)
 def custom() -> alt.theme.ThemeConfig:
+    colors = {
+        "DKolors": ["#4BA69F", "#C6A639", "#75954A", "#4A8A95", "#FCF6D1"],
+        "ordinal": ["#585D5E", "#4BA69F"],
+        "diverging": ["#D6E2E9", "#4BA69F"]
+    }
     opts = alt.theme.options
     return {
-        "background": None if opts["transparentBackground"] or opts["darkmode"] else opts["chartBackgroundColor"], # background of the entire view
+        "background": None
+        if opts["transparentBackground"] or opts["darkmode"]
+        else opts["chartBackgroundColor"],  # background of the entire view
         "config": {
             "area": {
-                'fill': opts['markFillColor'],
-                'fillOpacity': opts['markFillOpacity'],
+                "fill": opts["markFillColor"],
+                "fillOpacity": opts["markFillOpacity"],
                 "stroke": "white" if opts["darkmode"] else opts["markStrokeColor"],
-                'strokeOpacity': opts['markStrokeOpacity'],
-                'strokeWidth': opts['markStrokeWidth'],
+                "strokeOpacity": opts["markStrokeOpacity"],
+                "strokeWidth": opts["markStrokeWidth"],
             },
             "axis": {
                 "domain": True,
                 "domainColor": "white" if opts["darkmode"] else "black",
                 "domainWidth": opts["axisWidth"],
                 "grid": opts["grid"],
-                "gridColor": opts["gridColor"] if opts["darkmode"] else opts["gridColor"],
+                "gridColor": opts["gridColor"]
+                if opts["darkmode"]
+                else opts["gridColor"],
                 "gridOpacity": 0.5,
                 "gridWidth": opts["axisWidth"],
                 "labelColor": "white" if opts["darkmode"] else "black",
@@ -105,24 +118,28 @@ def custom() -> alt.theme.ThemeConfig:
                 "titleFont": opts["font"],
                 "titleFontStyle": opts["fontStyle"],
                 "titleFontWeight": opts["fontWeight"],
-                "translate": 0, # default is 0.5, which causes x and y axes to be misaligned / shifted. Required for top and right border alignment.
+                "translate": 0,  # default is 0.5, which causes x and y axes to be misaligned / shifted. Required for top and right border alignment.
             },
             "axisX": {
-                "labelAlign": "right" if opts["angledX"] else "center", # keep label alignment distinct between X & Y
+                "labelAlign": "right"
+                if opts["angledX"]
+                else "center",  # keep label alignment distinct between X & Y
                 "labelAngle": 315 if opts["angledX"] else 0,
                 "ticks": True if opts["xTicks"] and opts["ticks"] else False,
             },
             "axisY": {
-                "labelAlign": "center" if opts["verticalY"] else "right", # keep label alignment distinct between X & Y
+                "labelAlign": "center"
+                if opts["verticalY"]
+                else "right",  # keep label alignment distinct between X & Y
                 "labelAngle": 270 if opts["verticalY"] else 0,
                 "ticks": True if opts["yTicks"] and opts["ticks"] else False,
             },
             "bar": {
-                'fill': opts['markFillColor'],
-                'fillOpacity': opts['markFillOpacity'],
+                "fill": opts["markFillColor"],
+                "fillOpacity": opts["markFillOpacity"],
                 "stroke": "white" if opts["darkmode"] else opts["markStrokeColor"],
-                'strokeOpacity': opts['markStrokeOpacity'],
-                'strokeWidth': opts['markStrokeWidth'],
+                "strokeOpacity": opts["markStrokeOpacity"],
+                "strokeWidth": opts["markStrokeWidth"],
             },
             "boxplot": {
                 "box": {
@@ -133,19 +150,21 @@ def custom() -> alt.theme.ThemeConfig:
                     "strokeWidth": opts["markStrokeWidth"],
                 },
                 "median": {
-                    "fill": "black" if opts["darkmode"] else opts["viewBackgroundColor"],
+                    "fill": "black"
+                    if opts["darkmode"]
+                    else opts["viewBackgroundColor"],
                     "fillOpacity": opts["markFillOpacity"],
                     "size": opts["markSize"],
                     "stroke": "white" if opts["darkmode"] else opts["markStrokeColor"],
                     "strokeOpacity": opts["markStrokeOpacity"],
                     "strokeWidth": opts["markStrokeWidth"],
                 },
-                "rule": { # may inherit undeclared fields from top-level rule config
+                "rule": {  # may inherit undeclared fields from top-level rule config
                     "fill": "white" if opts["darkmode"] else "black",
                     "fillOpacity": opts["markFillOpacity"],
                     "size": opts["markSize"],
                     "stroke": "white" if opts["darkmode"] else opts["markStrokeColor"],
-                    "strokeDash": [0,0],
+                    "strokeDash": [0, 0],
                     "strokeOpacity": opts["markStrokeOpacity"],
                     "strokeWidth": opts["markStrokeWidth"],
                 },
@@ -161,10 +180,10 @@ def custom() -> alt.theme.ThemeConfig:
             },
             "circle": {
                 "fill": opts["markFillColor"],
-                "fillOpacity": opts["markFillOpacity"] / 2,
+                "fillOpacity": opts["markFillOpacity"],
                 "size": opts["markSize"],
                 "stroke": opts["markStrokeColor"],
-                "strokeOpacity": opts["markStrokeOpacity"] / 2,
+                "strokeOpacity": opts["markStrokeOpacity"],
                 "strokeWidth": opts["markStrokeWidth"],
             },
             "font": opts["font"],
@@ -177,18 +196,17 @@ def custom() -> alt.theme.ThemeConfig:
                 "titleFont": opts["font"],
                 "titleFontStyle": opts["fontStyle"],
                 "titleFontWeight": opts["fontWeight"],
-                "titlePadding": 0
+                "titlePadding": 0,
             },
             "legend": {
                 "disable": not opts["legend"],
                 "gradientOpacity": opts["markFillOpacity"],
+                "gradientStrokeColor": "white" if opts["darkmode"] else "black",
                 "gradientStrokeWidth": opts["markStrokeWidth"],
                 "labelColor": "white" if opts["darkmode"] else "black",
                 "labelFont": opts["font"],
                 "labelFontStyle": opts["fontStyle"],
                 "labelFontWeight": opts["fontWeight"],
-                "gradientStrokeColor": "white" if opts["darkmode"] else "black",
-                "gradientStrokeWidth": opts["axisWidth"],
                 "strokeColor": "white" if opts["darkmode"] else "black",
                 "strokeWidth": opts["axisWidth"] if opts["legendStroke"] else 0,
                 "symbolStrokeColor": "white" if opts["darkmode"] else "black",
@@ -206,24 +224,23 @@ def custom() -> alt.theme.ThemeConfig:
             },
             "point": {
                 "fill": opts["markFillColor"],
-                "fillOpacity": opts["markFillOpacity"] / 2,
+                "fillOpacity": opts["markFillOpacity"],
                 "size": opts["markSize"],
                 "stroke": opts["markStrokeColor"],
-                "strokeOpacity": opts["markStrokeOpacity"] / 2,
+                "strokeOpacity": opts["markStrokeOpacity"],
                 "strokeWidth": opts["markStrokeWidth"],
             },
             "range": {
-                "category": {
-                    "scheme": "greys",
-                },
+                # pass in a list outside of a dict to AVOID interpolations; define as {scheme: _} to USE interpolation, which will NOT use the maximum range of colors
+                "category": colors["DKolors"],
                 "diverging": {
-                    "scheme": "purplegreen",
+                    "scheme": colors["diverging"],
                 },
                 "heatmap": {
                     "scheme": "plasma" if opts["darkmode"] else "viridis",
                 },
                 "ordinal": {
-                    "scheme": "greys",
+                    "scheme": colors["ordinal"],
                 },
                 "ramp": {
                     "scheme": "plasma" if opts["darkmode"] else "viridis",
@@ -237,17 +254,17 @@ def custom() -> alt.theme.ThemeConfig:
                 "strokeWidth": opts["axisWidth"],
             },
             "rect": {
-                'fill': opts['markFillColor'],
-                'fillOpacity': opts['markFillOpacity'],
+                "fill": opts["markFillColor"],
+                "fillOpacity": opts["markFillOpacity"],
                 "stroke": "white" if opts["darkmode"] else opts["markStrokeColor"],
-                'strokeOpacity': opts['markStrokeOpacity'],
-                'strokeWidth': opts['markStrokeWidth'],
+                "strokeOpacity": opts["markStrokeOpacity"],
+                "strokeWidth": opts["markStrokeWidth"],
             },
             "square": {
                 "fill": opts["markFillColor"],
                 "fillOpacity": opts["markFillOpacity"],
                 "size": opts["markSize"],
-                "stroke": "white" if opts["darkmode"] else opts["markStrokeColor"],
+                "stroke": opts["markStrokeColor"],
                 "strokeOpacity": opts["markStrokeOpacity"],
                 "strokeWidth": opts["markStrokeWidth"],
             },
@@ -262,13 +279,13 @@ def custom() -> alt.theme.ThemeConfig:
                 "subtitleFontWeight": opts["fontWeight"],
             },
             "view": {
-                # 'fill': None,
-                'fill': None if opts["transparentBackground"] or opts["darkmode"] else opts["viewBackgroundColor"],
-                # 'continuousWidth': 300,
-                # 'continuousHeight': 300,
-                # 'discreteWidth': 20,
+                "fill": None
+                if opts["transparentBackground"] or opts["darkmode"]
+                else opts["viewBackgroundColor"],
                 "stroke": "white" if opts["darkmode"] else "black",
-                "strokeOpacity": 1 if opts["topAndRightBorder"] else 0,  # remove top and right axis borders
+                "strokeOpacity": 1
+                if opts["topAndRightBorder"]
+                else 0,  # remove top and right axis borders
                 "strokeWidth": opts["axisWidth"],
             },
         },
@@ -280,4 +297,8 @@ TO-DO LIST:
 - Try to add default paddingOutter and paddingInner values for all types of charts/marks.
 - Add support for area marks.
     - Add support for gradients - linear and maybe radial.
+- Figure out why opacity decreases (or does color change?) for area marks when line = True.
+- Figure out why the Y axis and X axis have dissimilar domain widths on large plots.
+- Maybe change legend scale size?
+- Add custom color palettes for both light and dark modes.
 """
