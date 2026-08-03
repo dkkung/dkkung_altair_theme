7ac80923dc6d1665e7f7dabcf97d82210ec91712
dkkung <128324842+dkkung@users.noreply.github.com>
2026-08-03 17:44:55 -0400

    change: restore australis on the field examples and drop the spiral galaxy

--- website/examples/interference.py
+++ website/examples/interference.py
@@ -7,7 +7,7 @@ import polars as pl
 import dysonsphere as ds
 
 alt.data_transformers.enable("default", max_rows=None)
-ds.theme(chartWidth=175, chartHeight=140, closed=True, viewPadding=False)
+ds.theme(chartWidth=175, chartHeight=140, heatmapPalette="australis", closed=True, viewPadding=False)
 
 N = 92
 W, H = 24.0, 18.0
